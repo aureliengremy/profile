4c4b4b77e0113ecf85715c7ba186b0b3b0ccd395
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-08-22 01:07:26 -0400

    write pseudo-code for sectionResume - add new icon for skills

--- src/components/Top/right/SectionResume.js
+++ src/components/Top/right/SectionResume.js
@@ -1,18 +1,32 @@
-import React, { } from 'react'
+import React, { Fragment } from 'react'
 import Accordion from '../../Accordion/Accordion'
 import SectionSkill from './SectionSkill'
 
 const SectionResume = ({ addClass }) => {
+
+    // add title for Exp.pro and Formation
+    // integrate list exp.pro and formation
+    // -> create accordion with title and description (skills used)
+    // -> 
+    // Pick up datas from Skill with function from SectionResume
+    // -> when accordion clicked call function handleClick
+    // -> handleClick get the list of skills
+    // -> send the list to SectionSkill to highlight icone
+
     return (
-        <div className={addClass}>
-            <div className="">
-                <Accordion />
+        <Fragment>
+            <div className={addClass}>
+                <div className="">
+                    <h3>Expérience professionnels</h3>
+                    <Accordion />
+                </div>
+                <div className="">
+                    <h3>Formation</h3>
+                    <Accordion />
+                </div>
             </div>
-            <div className="">
-                <Accordion />
-            </div>
-        <SectionSkill addClass="py-3" />
-        </div>
+            <SectionSkill addClass="py-3" />
+        </Fragment>
     )
 }
 
--- src/components/Top/right/SectionSkill.js
+++ src/components/Top/right/SectionSkill.js
@@ -11,26 +11,59 @@ import raspberry from '../../../assets/comp-icones/raspberry.png'
 import react from '../../../assets/comp-icones/react.png'
 import ruby from '../../../assets/comp-icones/ruby.png'
 import sass from '../../../assets/comp-icones/sass.png'
+import bootstrap from '../../../assets/comp-icones/bootstrap.png'
+import git from '../../../assets/comp-icones/git.png'
+import github from '../../../assets/comp-icones/github.png'
+import gitlab from '../../../assets/comp-icones/gitlab.png'
+import heroku from '../../../assets/comp-icones/heroku.png'
+import intellij from '../../../assets/comp-icones/intellij.png'
+import java from '../../../assets/comp-icones/java.png'
+import sql from '../../../assets/comp-icones/sql.png'
+import tailwind from '../../../assets/comp-icones/tailwind-css.png'
+import vscode from '../../../assets/comp-icones/vscode.png'
+import CardSkill from '../../Card/CardSkill'
 import Skill from './Skill'
 
 
-const SectionSkill = ({addClass}) => {
-   
+const SectionSkill = ({ addClass }) => {
+
     return (
-        <div className={`section-Skill ${addClass}`}>
+        <div className={`section-skill ${addClass}`}>
             <h3>Compétence</h3>
-            <Skill iconeSrc={illustrator} />
-            <Skill iconeSrc={photoshop} />
-            <Skill iconeSrc={xd} />
-            <Skill iconeSrc={figma} />
-            <Skill iconeSrc={html} />
-            <Skill iconeSrc={javascript} />
-            <Skill iconeSrc={linux} />
-            <Skill iconeSrc={notion} />
-            <Skill iconeSrc={raspberry} />
-            <Skill iconeSrc={react} />
-            <Skill iconeSrc={ruby} />
-            <Skill iconeSrc={sass} />
+            <CardSkill>
+                <h4>Language</h4>
+                <Skill iconeSrc={html} />
+                <Skill iconeSrc={sass} />
+                <Skill iconeSrc={ruby} />
+                <Skill iconeSrc={javascript} />
+                <Skill iconeSrc={react} />
+                <Skill iconeSrc={git} />
+                <Skill iconeSrc={java} />
+                <Skill iconeSrc={sql} />
+                <Skill iconeSrc={tailwind} />
+            </CardSkill>
+            <CardSkill>
+                <h4>Tools</h4>
+                <Skill iconeSrc={linux} />
+                <Skill iconeSrc={notion} />
+                <Skill iconeSrc={raspberry} />
+                <Skill iconeSrc={github} />
+                <Skill iconeSrc={gitlab} />
+                <Skill iconeSrc={heroku} />
+                <Skill iconeSrc={intellij} />
+                <Skill iconeSrc={vscode} />
+            </CardSkill>
+            <CardSkill>
+                <h4>Design</h4>
+                <Skill iconeSrc={illustrator} />
+                <Skill iconeSrc={photoshop} />
+                <Skill iconeSrc={xd} />
+                <Skill iconeSrc={figma} />
+            </CardSkill>
+
+
+
+
         </div>
     )
 }
