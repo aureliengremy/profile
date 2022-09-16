375fccabe032ab2ba31632d7c4051698790a2570
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-09-16 16:56:46 -0400

    building the middle part with all the projects in Card clickable

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const Card = ({text, link}) => {
+const Card = ({title, text, image, link}) => {
+
     return (
-        <div className="flex items-center rounded-lg bg-white shadow-lg overflow-hidden">
-            {/* <img className="h32 w-32 flex-shrink-0" src={destination.imageUrl} alt={destination.imageAlt} />
+        <Fragment>
+        <div className="flex items-center rounded-lg bg-white shadow-lg overflow-hidden" >
+            <img className="h32 w-32 flex-shrink-0" src={image} alt={title} />
             <div className="px-6 py-4">
-                <h3 className="text-lg font-semibold text-gray-800">{destination.city}</h3>
+                <h3 className="text-lg font-semibold text-gray-800">{text}</h3>
 
-                <p className="text-gray-600">${destination.averagePrice} / night average</p>
-                <div className="mt-4">
-                    <a href="/" className="text-indigo-600 hover:text-indigo-500 font-semibold text-sm">
-                        Explore {destination.propertyCount} properties
-                    </a>
-                </div>
-            </div> */}
-            {text}
-            {link}
+            
+            </div>
+            {/* {text}
+            {link} */}
         </div>
+
+        </Fragment>
+
     )
 }
 
--- src/components/Middle/SectionProject.js
+++ src/components/Middle/SectionProject.js
@@ -1,7 +1,62 @@
 import React from 'react'
 import Card from '../Card/Card'
+import GMC from '../../assets/images/website-screen/Grenier_Chevrolet_Buick_GMC.png';
+import Honda from '../../assets/images/website-screen/Lallier_Ste-Foy_Honda.png';
+import Marvel from '../../assets/images/website-screen/Marvel_Quiz_App.png';
+import NightSkyper from '../../assets/images/website-screen/NightSkyper.png';
+import RentALife from '../../assets/images/website-screen/RentALife.png';
+import Toyota from '../../assets/images/website-screen/Toyota_Lachute.png';
 
 const Project = () => {
+
+    const ProjectsData = [
+        {
+            name: 'Lallier Ste-Foy - Honda',
+            img: Honda,
+            text: (
+                <div>
+                    <p>Développement front-end, production de site-web personnalisé, gestion des environnements et déploiement de plateformes en ligne</p>
+                    <ul>
+                        <li> Amélioration de support visuel et applicatif</li>
+                        <li> Analyse, correction d'anomalies et versionnage du code</li>
+                        <li>Développement d’évolutions du framework et CRM</li>
+                    </ul>
+                    <p>J'ai effectué des tâches/intégration/fix, sur plus de 50 sites différents.</p>
+                </div>
+            ),
+            link: 'https://www.lallierstefoy.com/fr'
+        },
+        {
+            name: 'Grenier - Chevrolet Buick GMC',
+            img: GMC,
+            text: 'lorem ipsum dolor sit amet, consectet',
+            link: 'https://www.grenierchevrolet.com/fr'
+        },
+        {
+            name: 'Lachute - Toyota',
+            img: Toyota,
+            text: 'lorem ipsum dolor sit amet, consectet',
+            link: 'https://www.toyotalachute.com/'
+        },
+        {
+            name: 'Quiz Marvel',
+            img: Marvel,
+            text: 'lorem ipsum dolor sit amet, consectet',
+            link: 'https://marvel-quiz-app-hosted.web.app/'
+        },
+        {
+            name: 'Rent-A-Life',
+            img: RentALife,
+            text: 'Clone d’Airbnb à partir de zéro en 5 jours. La production est déployée sur Heroku en respectant un workflow efficace et standard. Apprentissage de fonctionnalités avancées telle que le paiement, image upload, search, geocoding…',
+            link: 'https://rent-a-life.herokuapp.com/'
+        },
+        {
+            name: 'NightSkyper',
+            img: NightSkyper,
+            text: 'Inventer, pitcher, concevoir, designer, coder et déployer d’un projet original d’application web en équipe en 2 semaines.',
+            link: 'http://www.nightskyper.xyz/'
+        },
+    ]
     const el = document.getElementsByClassName("section-project")
     if (el) {
         // el.getBoundingClientRect().top
@@ -10,9 +65,14 @@ const Project = () => {
         <div className="section-project mx-auto py-8">
             <h3 className="text-xl text-gray-900">Project</h3>
             <div className="mt-6 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 xl:gap-6">
-                <Card text="card 1" link="https://rent-a-life.herokuapp.com/"/>
-                <Card text="card 2" link="https://rent-a-life.herokuapp.com/"/>
-                <Card text="card 3" link="https://rent-a-life.herokuapp.com/"/>
+                {ProjectsData.map((project,index) => (
+                    <Card   title={project.name}   
+                            text={project.text}
+                            image={project.img} 
+                            link={project.link} />
+                    
+                ))}
+
             </div>
         </div>
     )
