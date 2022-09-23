0567a87ff04e44f72ab51d94c92eed8604b9d07b
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-09-22 22:26:39 -0400

    modify leftPart bgImage

--- src/components/Top/left/Left.js
+++ src/components/Top/left/Left.js
@@ -7,8 +7,7 @@ import { useState } from 'react';
 const Left = ({pourcScroll}) => {
 
     const [leftStyle, setLeftStyle] = useState({
-        backgroundSize: "70vh",
-        transitionProperty: "background-position",
+        backgroundSize: "75vh",
         transitionDuration: "3s",
         backgroundPosition: "left 0px"
     })
@@ -24,9 +23,7 @@ const Left = ({pourcScroll}) => {
         <div className="left backdrop-blur-sm relative lg:h-full lg:w-1/3"
              style={leftStyle}>
             <NavLink to='/old_version' className="old_version" alt="old-version">📁</NavLink>
-            {/* <div className="max-w-md mx-auto py-5" style={{marginTop:`${marginCustom}`}}> */}
             <div className='max-w-md mx-auto py-5 lg:mt-64'>
-                {/* scroll change marginTop value */}
                 <Profile />
                 <Social />
             </div>
